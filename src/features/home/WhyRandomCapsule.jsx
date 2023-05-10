ed32a4900d6d811575f6d8b94de55a2e6977d0b7
Raman Sharma <raman.s.sharma@oracle.com>
2023-05-10 15:53:18 +0530

    Why Random Capsule changes

---
+++ src/features/home/WhyRandomCapsule.jsx
@@ -0,0 +1,142 @@
+const WhyRandomCapsule = ({ className }) => {
+  return (
+    <div className={`bg-white py-6 sm:py-8 lg:py-12 ${className}`}>
+      <div class="mx-auto max-w-screen-2xl px-4 md:px-8">
+        <div class="mb-10 md:mb-16">
+          <h2 class="mb-4 text-center text-4xl font-bold text-gray-800 md:mb-6 lg:text-5xl">
+            Why Random Capsule?
+          </h2>
+
+          <p class="mx-auto max-w-screen-md text-center text-gray-500 md:text-lg">
+            Random Capsule has been made with the sole aim to bring afforadable
+            and better mental health-care closer to you!
+          </p>
+        </div>
+        <div class="grid gap-3 sm:grid-cols-2 md:gap-8 xl:grid-cols-2">
+          <div class="flex divide-x rounded-lg border bg-gray-50">
+            <div class="flex items-center p-2 text-indigo-500 md:p-4">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-6 w-6 md:h-8 md:w-8"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M5 13l4 4L19 7"
+                />
+              </svg>
+            </div>
+
+            <div class="p-4 md:p-6">
+              <h3 class="mb-2 text-lg font-semibold md:text-xl">
+                Personalized Support
+              </h3>
+              <p class="text-gray-500">
+                We understand that everyone's journey is different, and we're
+                here to provide you with the guidance and assistance that fits
+                your specific goals.
+              </p>
+            </div>
+          </div>
+          <div class="flex divide-x rounded-lg border bg-gray-50">
+            <div class="flex items-center p-2 text-indigo-500 md:p-4">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-6 w-6 md:h-8 md:w-8"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M5 13l4 4L19 7"
+                />
+              </svg>
+            </div>
+
+            <div class="p-4 md:p-6">
+              <h3 class="mb-2 text-lg font-semibold md:text-xl">
+                Flexibilty of Sessions
+              </h3>
+              <p class="text-gray-500">
+                Whether you prefer face-to-face consultations, online video
+                sessions, or prefer to chat, our platform offers convenient
+                options that allow you to receive the support you need from the
+                comfort of your own environment.
+              </p>
+            </div>
+          </div>
+
+          <div class="flex divide-x rounded-lg border bg-gray-50">
+            <div class="flex items-center p-2 text-indigo-500 md:p-4">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-6 w-6 md:h-8 md:w-8"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M5 13l4 4L19 7"
+                />
+              </svg>
+            </div>
+
+            <div class="p-4 md:p-6">
+              <h3 class="mb-2 text-lg font-semibold md:text-xl">
+                Extensive Practical Experience
+              </h3>
+              <p class="text-gray-500">
+                Experience the valuable expertise and extensive practical
+                knowledge of Ms. Pooja, empowering her to offer you impactful
+                strategies and evidence-based approaches that can truly
+                transform your life.
+              </p>
+            </div>
+          </div>
+
+          <div class="flex divide-x rounded-lg border bg-gray-50">
+            <div class="flex items-center p-2 text-indigo-500 md:p-4">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-6 w-6 md:h-8 md:w-8"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M5 13l4 4L19 7"
+                />
+              </svg>
+            </div>
+
+            <div class="p-4 md:p-6">
+              <h3 class="mb-2 text-lg font-semibold md:text-xl">
+                Afforadable Pricing
+              </h3>
+              <p class="text-gray-500">
+                Prioritizing affordability without compomising quality, we aim
+                to ensure that you can receive the support you deserve without
+                financial barriers standing in the way.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default WhyRandomCapsule;
--- src/features/pages/home/index.jsx
+++ src/features/pages/home/index.jsx
@@ -1,49 +1,55 @@
 import { Link } from "react-router-dom";
 import AnimatedHomeTitle from "../../common/AnimatedHomeTitle";
+import WhyRandomCapsule from "../../home/WhyRandomCapsule";
 import SEO from "../../seo";
 
 const Home = () => {
   return (
-    <div className="flex py-10 text-black w-full min-h-screen justify-around">
-      <SEO title="Home" />
-      <div className="text-gray-600 body-font">
-        <div className="container mx-auto flex px-5 md:flex-row flex-col items-center">
-          <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0 flex justify-center">
-            <img
-              className="md:w-3/4 lg:w-full  w-1/2 h-auto"
-              src={require("../../../assets/img/pooja.png")}
-              alt="Pooja Gupta"
-            />
-          </div>
-          <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
-            <div className="flex justify-center ">
-              <AnimatedHomeTitle />
+    <>
+      <div className="flex py-10 text-black w-full min-h-screen justify-around">
+        <SEO title="Home" />
+        <div className="text-gray-600 body-font">
+          <div className="container mx-auto flex px-5 md:flex-row flex-col items-center">
+            <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0 flex justify-center">
+              <img
+                className="md:w-3/4 lg:w-full  w-1/2 h-auto"
+                src={require("../../../assets/img/pooja.png")}
+                alt="Pooja Gupta"
+              />
             </div>
-            <p className="my-2 mb-8 leading-relaxed sm:mr-20">
-              Hello! I'm <b>Pooja Gupta</b>, a psychologist specialising in
-              Clinical Psychology. <br /> <br /> My mission is to bring
-              afforadable and the best possible mental healthcare to everyone. I
-              am with you in this journey of healing and loving yourself a
-              little bit more. Don't worry, I've got your back!
-            </p>
-            <div className="flex justify-center">
-              <Link
-                to="/your-psychologist"
-                className="inline-flex text-white bg-black py-2 px-6 focus:outline-none hover:bg-gray-800 rounded text-lg"
-              >
-                Know more &#8594;
-              </Link>
-              <Link
-                to="/booking"
-                className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg"
-              >
-                Book Session
-              </Link>
+            <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
+              <div className="flex justify-center ">
+                <AnimatedHomeTitle />
+              </div>
+              <p className="my-2 mb-8 leading-relaxed sm:mr-20">
+                Hello! I'm <b>Pooja Gupta</b>, a psychologist specialising in
+                Clinical Psychology. <br /> <br /> My mission is to bring
+                afforadable and the best possible mental healthcare to everyone.
+                I am with you in this journey of healing and loving yourself a
+                little bit more. Don't worry, I've got your back!
+              </p>
+              <div className="flex justify-center">
+                <Link
+                  to="/your-psychologist"
+                  className="inline-flex text-white bg-black py-2 px-6 focus:outline-none hover:bg-gray-800 rounded text-lg"
+                >
+                  Know more &#8594;
+                </Link>
+                <Link
+                  to="/booking"
+                  className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg"
+                >
+                  Book Session
+                </Link>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+      <div>
+        <WhyRandomCapsule className={"mt-5 px-14"}/>
+      </div>
+    </>
   );
 };
 
